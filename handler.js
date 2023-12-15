537688cc60687758633aff6069847052a5406eb7
sudhnitk <sudhnit.kaur@ellucian.com>
2023-12-15 15:25:02 +0530

    Updated version

--- handler.js
+++ handler.js
@@ -1,10 +1,16 @@
 const { FirehoseClient, PutRecordCommand } = require('@aws-sdk/client-firehose');
 const { fromUtf8 } = require('@aws-sdk/util-utf8-node');
 
-exports.sendToFirehose = async (event, context) => {
-    const firehoseClient = new FirehoseClient({ region: 'us-east-1' }); // Replace with your region
+exports.sendToFirehose = async (event) => {
+    const firehoseClient = new FirehoseClient({ region: 'us-east-1' }); 
 
     console.log('Received event:', event);
+
+   
+    if (!event.body) {
+        console.error('Missing or empty request body');
+        return { statusCode: 400, body: 'Missing or empty request body' };
+    }
 
     let requestBody;
     try {
@@ -12,13 +18,13 @@ exports.sendToFirehose = async (event, context) => {
         console.log('Parsed request body:', requestBody);
     } catch (error) {
         console.error('Error parsing request body:', error);
-        return { statusCode: 400, body: 'Invalid request body' };
+        return { statusCode: 400, body: 'Invalid JSON format in request body' };
     }
 
     const params = {
-        DeliveryStreamName: 'PUT-S3-EBGEB', 
+        DeliveryStreamName: 'PUT-S3-yVzRP', 
         Record: {
-            Data: fromUtf8(JSON.stringify(requestBody)) 
+            Data: fromUtf8(JSON.stringify(requestBody))
         }
     };
 
